105edf4d35f5b6779eeafa2df5e090ce9e2dd3cc
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-03-22 16:40:31 +0700

    Fix Name resposne of Rating

--- src/PlayTogether.Core/Dtos/Outcoming/Business/Rating/UserRateResponse.cs
+++ src/PlayTogether.Core/Dtos/Outcoming/Business/Rating/UserRateResponse.cs
@@ -3,7 +3,7 @@ namespace PlayTogether.Core.Dtos.Outcoming.Business.Rating
     public class UserRateResponse
     {
         public string Id { get; set; }
-        public string FirstName { get; set; }
+        public string Name { get; set; }
         public string Avartar { get; set; }
     }
 }
