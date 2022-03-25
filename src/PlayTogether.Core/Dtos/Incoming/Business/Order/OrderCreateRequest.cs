f3c7a055a5b46a59e4ab5aceb52f4e4de7d7a24a
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-03-25 11:00:13 +0700

    Remove total time validate

--- src/PlayTogether.Core/Dtos/Incoming/Business/Order/OrderCreateRequest.cs
+++ src/PlayTogether.Core/Dtos/Incoming/Business/Order/OrderCreateRequest.cs
@@ -9,7 +9,6 @@ namespace PlayTogether.Core.Dtos.Incoming.Business.Order
         public DateTime CreatedDate = DateTime.Now;
 
         [Required]
-        [Range(1, 3)]
         public int TotalTimes { get; set; }
 
         [MaxLength(100)]
