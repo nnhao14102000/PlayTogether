7ec49223816a99c8882700f1b8c26aa4e2e49f68
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-03-29 13:45:30 +0700

    Add Id for User in get user in admin

--- src/PlayTogether.Core/Dtos/Outcoming/Business/AppUser/UserGetByAdminResponse.cs
+++ src/PlayTogether.Core/Dtos/Outcoming/Business/AppUser/UserGetByAdminResponse.cs
@@ -4,6 +4,7 @@ namespace PlayTogether.Core.Dtos.Outcoming.Business.AppUser
 {
     public class UserGetByAdminResponse
     {
+        public string Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public DateTime CreatedDate { get; set; }
