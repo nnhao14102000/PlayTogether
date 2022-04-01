56e5560d703fbf14708c22336339a5c24494a3e0
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-04-01 20:42:42 +0700

    Fix param of get all user for admin

--- src/PlayTogether.Core/Parameters/AdminUserParameters.cs
+++ src/PlayTogether.Core/Parameters/AdminUserParameters.cs
@@ -3,7 +3,7 @@ namespace PlayTogether.Core.Parameters
     public class AdminUserParameters : QueryStringParameters
     {
         public string Name { get; set; }
-        public bool IsActive { get; set; }
+        public bool? IsActive { get; set; }
         public string Status { get; set; }
     }
 }
