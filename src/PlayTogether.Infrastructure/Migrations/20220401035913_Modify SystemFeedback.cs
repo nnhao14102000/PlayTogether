ce90593cd2de9518f14be7ef721abc5078c3c404
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-04-01 11:01:19 +0700

    Migration modify in SystemFeedback

---
+++ src/PlayTogether.Infrastructure/Migrations/20220401035913_Modify SystemFeedback.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PlayTogether.Infrastructure.Migrations
+{
+    public partial class ModifySystemFeedback : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsApprove",
+                table: "SystemFeedback",
+                type: "bit",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsApprove",
+                table: "SystemFeedback");
+        }
+    }
+}
