c218b5b6ea9d2e8e3321ea42475a0629ffd96803
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-04-02 09:06:58 +0700

    Format code, remove comment...

--- src/PlayTogether.Core/Dtos/Incoming/Generic/UserStatusConstants.cs
+++ src/PlayTogether.Core/Dtos/Incoming/Generic/UserStatusConstants.cs
@@ -2,8 +2,6 @@ namespace PlayTogether.Core.Dtos.Incoming.Generic
 {
     public class UserStatusConstants
     {
-        public const string NotAcceptPolicy = "NotAcceptPolicy";
-        public const string AcceptPolicy = "AcceptPolicy";
         public const string Online = "Online";
         public const string Offline = "Offline";
         public const string Processing = "Processing";
