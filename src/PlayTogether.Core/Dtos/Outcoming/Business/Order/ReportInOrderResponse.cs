aa45a3e150fee2ed1c50ae3cd6ee48a03335e060
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-04-09 10:04:54 +0700

    Update report response

--- src/PlayTogether.Core/Dtos/Outcoming/Business/Order/ReportInOrderResponse.cs
+++ src/PlayTogether.Core/Dtos/Outcoming/Business/Order/ReportInOrderResponse.cs
@@ -6,7 +6,7 @@ namespace PlayTogether.Core.Dtos.Outcoming.Business.Order
     {
         public string Id { get; set; }
         public string ReportMessage { get; set; }
-        public bool IsApprove { get; set; }
+        public bool? IsApprove { get; set; }
         public DateTime CreatedDate { get; set; }
     }
 }
