11793c05e692c3aebe3ba4dc4362d0c674c304c6
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-04-11 16:41:07 +0700

    Update maxPageSize

--- src/PlayTogether.Core/Parameters/QueryStringParameters.cs
+++ src/PlayTogether.Core/Parameters/QueryStringParameters.cs
@@ -2,7 +2,7 @@
 {
     public class QueryStringParameters
     {
-        const int maxPageSize = 50;
+        const int maxPageSize = 1000;
         public int PageNumber { get; set; } = 1;
 
         private int _pageSize = 10;
