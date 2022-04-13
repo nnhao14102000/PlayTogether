fdd6b7db19a9035015c8576556cd279262052582
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-04-13 17:06:37 +0700

    Add Mapper Dating

--- src/PlayTogether.Infrastructure/Helpers/MapperProfiles.cs
+++ src/PlayTogether.Infrastructure/Helpers/MapperProfiles.cs
@@ -176,6 +176,7 @@ namespace PlayTogether.Infrastructure.Helpers
 
             // Dating mapper profile
             CreateMap<DatingCreateRequest, Dating>();
+            CreateMap<DatingUpdateRequest, Dating>();
             CreateMap<Dating, DatingUserResponse>();
 
             // Charity with draw
