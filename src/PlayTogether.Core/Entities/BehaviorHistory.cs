a16116422147b4f318f32f652b44879725270ed9
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-04-15 16:06:54 +0700

    Update Db Behavior

--- src/PlayTogether.Core/Entities/BehaviorHistory.cs
+++ src/PlayTogether.Core/Entities/BehaviorHistory.cs
@@ -8,10 +8,19 @@ namespace PlayTogether.Core.Entities
         [MaxLength(100)]
         public string BehaviorPointId { get; set; }
         public BehaviorPoint BehaviorPoint { get; set; }
+
+        [MaxLength(10)]
         public string Operation { get; set; }
+
+        [MaxLength(100)]
         public string Type { get; set; }
+
         public int Num { get; set; }
+
+        [MaxLength(100)]
         public string TypePoint { get; set; }
+        
+        [MaxLength(100)]
         public string ReferenceBehaviorId { get; set; }
     }
 }
---
+++ src/PlayTogether.Infrastructure/Migrations/20220415090424_Modify Behavior History.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PlayTogether.Infrastructure.Migrations
+{
+    public partial class ModifyBehaviorHistory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TypePoint",
+                table: "BehaviorHistories",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "BehaviorHistories",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ReferenceBehaviorId",
+                table: "BehaviorHistories",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Operation",
+                table: "BehaviorHistories",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TypePoint",
+                table: "BehaviorHistories",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "BehaviorHistories",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ReferenceBehaviorId",
+                table: "BehaviorHistories",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Operation",
+                table: "BehaviorHistories",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10,
+                oldNullable: true);
+        }
+    }
+}
