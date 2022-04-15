2d5d4b46de9cbf72341dd0a498b6ef908007ec56
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-04-15 17:12:35 +0700

    Update TransactionHistory

--- src/PlayTogether.Core/Entities/TransactionHistory.cs
+++ src/PlayTogether.Core/Entities/TransactionHistory.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PlayTogether.Core.Entities
 {
@@ -8,11 +9,15 @@ namespace PlayTogether.Core.Entities
         [MaxLength(100)]
         public string UserBalanceId { get; set; }
         public UserBalance UserBalance { get; set; }
+
         [Required]
         [MaxLength(10)]
         public string Operation { get; set; }
+
         [Required]
+        [Column(TypeName = "float")]
         public float Money { get; set; }
+        
         [Required]
         [MaxLength(100)]
         public string TypeOfTransaction { get; set; }
---
+++ src/PlayTogether.Infrastructure/Migrations/20220415101115_Modify TransactionHistory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PlayTogether.Infrastructure.Migrations
+{
+    public partial class ModifyTransactionHistory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Money",
+                table: "TransactionHistories",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Money",
+                table: "TransactionHistories",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
