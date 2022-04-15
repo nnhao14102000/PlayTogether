9becb2f285b5ccd05b2b617a28338e0d980177e8
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-04-15 16:17:10 +0700

    Update contraint

--- src/PlayTogether.Core/Dtos/Incoming/Business/TransactionHistory/DepositRequest.cs
+++ src/PlayTogether.Core/Dtos/Incoming/Business/TransactionHistory/DepositRequest.cs
@@ -5,8 +5,10 @@ namespace PlayTogether.Core.Dtos.Incoming.Business.TransactionHistory
     public class DepositRequest
     {
         [Required]
+        [MaxLength(100)]
         public string MomoTransactionId { get; set; }
         [Required]
+        [Range(0, float.MaxValue)]
         public float Money { get; set; }
     }
 }
