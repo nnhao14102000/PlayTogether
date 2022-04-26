79689b972ebbe4c2efb868a53c120877f8781df6
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-04-26 10:34:27 +0700

    Update constant

--- src/PlayTogether.Core/Dtos/Incoming/Generic/ErrorMessageConstants.cs
+++ src/PlayTogether.Core/Dtos/Incoming/Generic/ErrorMessageConstants.cs
@@ -9,7 +9,7 @@ namespace PlayTogether.Core.Dtos.Incoming.Generic
         public const string NotOwnInfo = "Thông tin không thuộc về sở hữu của bạn.";
         public const string Disable = "Tài khoản của bạn đang bị khóa. Vui lòng quay lại sau.";
         public const string DisableUser = "Tài khoản người dùng này đang bị khóa. Vui lòng quay lại sau.";
-        public const string Offline = "Tài khoản của bạng đang Offline. Vui lòng đăng nhập lại.";
+        public const string Offline = "Tài khoản của bạn đang Offline. Vui lòng đăng nhập lại.";
         public const string ServerError = "Lỗi máy chủ";
         public const string SaveChangesFailed = "Không lưu nhưng thay đổi được. Vui lòng thử lại.";
         public const string NotReady = "Tài khoản không sẵn sàng cho thao tác này.";
