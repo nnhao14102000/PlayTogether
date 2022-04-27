8c24ce8dbc73936c20ebcc9ed9564b9d3ecfbd5c
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-04-27 19:31:00 +0700

    Update finish soon order require reason

--- src/PlayTogether.Core/Dtos/Incoming/Business/Order/FinishRequest.cs
+++ src/PlayTogether.Core/Dtos/Incoming/Business/Order/FinishRequest.cs
@@ -5,6 +5,7 @@ namespace PlayTogether.Core.Dtos.Incoming.Business.Order
     public class FinishSoonRequest
     {
         [MaxLength(200)]
+        [Required]
         public string Reason { get; set; }
     }
 }
