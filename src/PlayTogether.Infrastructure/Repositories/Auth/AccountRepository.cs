75205faead4a73820226abcc0fe84d4a99b67fe2
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-04-29 07:35:12 +0700

    Update login user with google

--- src/PlayTogether.Infrastructure/Repositories/Auth/AccountRepository.cs
+++ src/PlayTogether.Infrastructure/Repositories/Auth/AccountRepository.cs
@@ -214,6 +214,7 @@ namespace PlayTogether.Infrastructure.Repositories.Auth
                         userEntityModel.IsPlayer = false;
                         userEntityModel.Status = UserStatusConstants.Online;
                         userEntityModel.Description = "";
+                        userEntityModel.City = "";
                         userEntityModel.PricePerHour = 10000;
 
                         await _context.AppUsers.AddAsync(userEntityModel);
