8a88d8b2d77bbb9d4ba4d9a91ec0e57abac1cce7
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-04-30 09:52:25 +0700

    Update statistic admin

--- src/PlayTogether.Infrastructure/Repositories/Business/Admin/AdminRepository.cs
+++ src/PlayTogether.Infrastructure/Repositories/Business/Admin/AdminRepository.cs
@@ -30,7 +30,7 @@ namespace PlayTogether.Infrastructure.Repositories.Business.Admin
             numOfReport = await _context.Reports.Where(x => x.IsApprove == null).CountAsync();
             numOfDisableUser = await _context.AppUsers.Where(x => x.IsActive == false).CountAsync();
             numOfSuggestFeedback = await _context.SystemFeedbacks
-                                                    .Where(x => x.IsApprove == null).CountAsync();
+                                                    .Where(x => x.IsApprove == -1).CountAsync();
             var users = await _context.AppUsers.ToListAsync();
             foreach (var item in users) {
                 var date = item.CreatedDate.ToShortDateString();
